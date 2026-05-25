d3cf89bcbe096306548133a31c418e6090b4e5dc
Nadya06 <nadnad0601@gmail.com>
2026-05-25 13:03:24 +0700

    Replace Supabase public key with new value
    Updated Supabase public key to a new value.

--- src/app/utils/supabaseClient.ts
+++ src/app/utils/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://wuilfieifgowpwykcofn.supabase.co"
 
-const supabasePublickey = "sb_publishable_nrrTZK0g1lc2uYQfFiFLqA_y8ES47E1"
+const supabasePublickey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1aWxmaWVpZmdvd3B3eWtjb2ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5MTk5MTAsImV4cCI6MjA5NDQ5NTkxMH0.I4uQqANoucLItpAJlhAGC9jjNDe0TaWLPHQoPrpXIAE"
 
 if (!supabaseUrl || !supabasePublickey) {
   throw new Error('Missing Supabase environment variables');
